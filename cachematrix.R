c64f09e26e5398492aa076b275a68348b80e0d80
Datagur <satyaki@icloud.com>
2015-01-25 13:38:51 -0500

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,37 @@
 ## The purpose of this script is to create a matrices and cache them. 
-## It checks if the inverse of it is computed and cached before re-calculating the inverse
+## It checks if the inverse of it is computed and cached before re-calculating 
+##the inverse
 
 ## This function creates a matirx with functions to create and cache
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse<- function(inverse) m <<-inverse
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse<- function(inverse) m <<-inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## This function returns the inverse of the matrix once created above. If avaialble it retrieves the matrix inverse
+## This function returns the inverse of the matrix once created above. 
+##If avaialble it retrieves the matrix inverse
 ## If not available it computes and returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if (!is.null(m)) {
-    message("get cached inv matrix")
-    return(m)
-  } else {
-    m <- solve(x$get())
-    x$setinverse(m)
-    return(m)
-  }
+        m <- x$getinverse()
+        if (!is.null(m)) {
+                message("get cached inv matrix")
+                return(m)
+        } else {
+                m <- solve(x$get())   # calculate the inverse
+                x$setinverse(m)
+                return(m)
+        }
 }
